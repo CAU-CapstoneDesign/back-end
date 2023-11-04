8a608c91148821401f2f22547fe7744518e21e3f
jeonghens <jhyeonseo0523@gmail.com>
2023-11-04 12:30:32 +0900

    Feat: obesity/admin.py ObesityHistory 등록

--- obesity/admin.py
+++ obesity/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Obesity
+from .models import Obesity, ObesityHistory
 
 # Register your models here.
 
 admin.site.register(Obesity)
+admin.site.register(ObesityHistory)
