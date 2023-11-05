b1c57ec50515a1d37eeb4391a8e86d155127d67b
jeonghens <jhyeonseo0523@gmail.com>
2023-11-05 18:55:50 +0900

    Feat: disease/models.py DiseaseHistory 수정

--- disease/models.py
+++ disease/models.py
@@ -12,7 +12,7 @@ class Disease(BaseModel):
     def __str__(self):
         return self.name
     
-class DiseaseHistory(BaseModel):
+class DiseaseHistory(models.Model):
     PARTS = (
         ('Head', '머리'),
         ('Body', '몸통'),
@@ -25,5 +25,5 @@ class DiseaseHistory(BaseModel):
     part = models.CharField(choices=PARTS, verbose_name='질환 의심 부위', max_length=10, null=False)
     result = models.JSONField(verbose_name='예측 결과', default=dict)
     # disease = models.ForeignKey(Disease, verbose_name='질병', on_delete=models.CASCADE)
-    diagnosis_date = models.DateField(verbose_name='진단 일자')
+    diagnosis_date = models.DateTimeField(verbose_name='진단 일자', auto_now=True)
     # explanation = models.TextField(verbose_name='보호자 기록', blank=True)
