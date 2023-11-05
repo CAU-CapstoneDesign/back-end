6c96541412d117b54229748b9bd85ae7d3345dce
jeonghens <jhyeonseo0523@gmail.com>
2023-11-05 14:10:13 +0900

    Feat: pet/models.py Pet name 필드 추가 및 수정

--- pet/models.py
+++ pet/models.py
@@ -20,6 +20,10 @@ class Pet(BaseModel):
 
     id = models.AutoField(primary_key=True)
     owner_id = models.ForeignKey(Owner, verbose_name='보호자', on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='이름', max_length=20, null=False)
     breed = models.CharField(choices=BREED, verbose_name='견종', max_length=20)
     gender = models.CharField(choices=GENDER, verbose_name='성별', max_length=20)
-    # photo = models.ImageField(upload_to='', blank=True, null=True)
+    # photo = models.ImageField(upload_to='', blank=True, null=True)        
+
+    def __str__(self):
+        return f'{self.owner_id}의 {self.name}'
